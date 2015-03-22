30c6026d607582aca34a0238e8cfef089ac65e1b
MugiwaraNoLuffy <redi.unipampa@gmail.com>
2015-03-22 16:43:52 -0300

    Retirada de comentarios desnecessarios

--- scripts/BarraAlertas.js
+++ scripts/BarraAlertas.js
@@ -12,39 +12,19 @@ function BarraAlertas()
 	{
 		var url ="scriptsDB/getAlert.php";
 		var temp = 1;
-		/*
-		$.ajax({
-			url: url,
-			cache: false,
-			dataType: "json",
-			success: 
-	
-		);
-		*/
 
 		$.getJSON(url, function(data){
 			var priority = 0;
 			for(var i=0; i<data.alerts.length; i++)
 			{
 				var object = data.alerts[i];
-//				alert();
 				if( parseInt(object.priority) > priority )
 				{
 					priority = parseInt(object.priority);
 				}
 			} 
 			var a = new BarraAlertas(); a.alerta(priority);
-/*			$.each(data.alerts, function(i, alarme){
-				temp = parseInt(alarme.priority);
-				var priority = 0;
-
-//				alert(priority);
-				if( temp > priority){ priority = temp; var a = new BarraAlertas(); a.alerta(temp);}
-//				alert(priority);
-			});*/
 		});
-
-	//	this.alerta(priority);
 	}
 
 	this.alerta = function (nivel)
@@ -61,7 +41,7 @@ function BarraAlertas()
 				$(barraId).css({"background-color": "#FF9900"});
 				break;
 			case VERMELHO:
-				$(barraId).css({"background-color": "#FF3300"});
+				$(barraId).css({"background-color": "#F43292"});
 				break;
 			case VERMELHOESC:
 				$(barraId).css({"background-color": "#FF0033"});
