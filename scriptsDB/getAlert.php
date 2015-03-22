70a50d41a265e03b5feb79c0b430fa9d79a0b3f5
MugiwaraNoLuffy <redi.unipampa@gmail.com>
2015-03-22 19:27:12 -0300

    Alerts zabbix - Inner Join

--- scriptsDB/getAlert.php
+++ scriptsDB/getAlert.php
@@ -6,24 +6,24 @@
 	$dbName="zabbix";
 
 	$link = mysqli_connect($host, $username, $password, $dbName);
-	$sql = "SELECT triggerid,description,priority FROM triggers WHERE value=1";
-
+//	$sql = "SELECT triggerid,description,priority FROM triggers WHERE value=1";
+	$sql = 	"SELECT triggers.description,triggers.priority,hosts.host,interface.ip FROM triggers INNER JOIN functions ON (triggers.triggerid=functions.triggerid) INNER JOIN items ON (functions.itemid=items.itemid) INNER JOIN hosts ON (items.hostid=hosts.hostid) INNER JOIN interface ON (interface.interfaceid=items.interfaceid) WHERE triggers.value=1";
 	$result = mysqli_query($link, $sql);
 	$alerts = array();
 	while( $row = mysqli_fetch_object($result) )
 	{
 		$alerts[] = $row;
+//		echo "$row->triggerid";
 	}
-/*	foreach($alerts as $al){
-		echo "$al->priority";
-	}
-*/	$json1 = '{"alerts":'.json_encode($alerts).'}';
+	$json1 = '{"alerts":'.json_encode($alerts).'}';
+//	echo serialize($alerts[0]);
 	echo "$json1";
+	echo "\n\0";
 	$alertas = json_decode($json1);
-	$aler = $alertas->alerts;
+
 /*	foreach( $aler as $a )
 	{
-		
+
 		echo "$a->priority";
 	}
 */
