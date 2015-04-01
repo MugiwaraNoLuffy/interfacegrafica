594ed8d774323a7fc1dae5c75a95795bbcb122eb
MugiwaraNoLuffy <redi.unipampa@gmail.com>
2015-04-01 12:09:59 -0700

    Gets and manipulate alerts from zabbix and nfdump.

--- scriptsDB/getAlert.php
+++ scriptsDB/getAlert.php
@@ -3,29 +3,49 @@
 	$host="localhost";
 	$username="root";
 	$password="root";
-	$dbName="zabbix";
+	$dbZabbix="zabbix";
+	$dbNfdump="nfdump";
 
-	$link = mysqli_connect($host, $username, $password, $dbName);
-//	$sql = "SELECT triggerid,description,priority FROM triggers WHERE value=1";
-	$sql = 	"SELECT triggers.description,triggers.priority,hosts.host,interface.ip FROM triggers INNER JOIN functions ON (triggers.triggerid=functions.triggerid) INNER JOIN items ON (functions.itemid=items.itemid) INNER JOIN hosts ON (items.hostid=hosts.hostid) INNER JOIN interface ON (interface.interfaceid=items.interfaceid) WHERE triggers.value=1";
+	/*
+	 * Carrega alertas zabbix
+	 */
+	$link = mysqli_connect($host, $username, $password, $dbZabbix);
+
+	$sql = 	"SELECT triggers.description,triggers.priority,hosts.host,hosts.hostid FROM triggers \
+			INNER JOIN functions ON (triggers.triggerid=functions.triggerid) \
+			INNER JOIN items ON (functions.itemid=items.itemid) \
+			INNER JOIN hosts ON (items.hostid=hosts.hostid) \
+			INNER JOIN interface ON (interface.interfaceid=items.interfaceid) \
+		WHERE triggers.value=1";
 	$result = mysqli_query($link, $sql);
 	$alerts = array();
 	while( $row = mysqli_fetch_object($result) )
 	{
+
+		$row->description =  str_replace("{HOST.NAME}", $row->host, $row->description );
+		unset($row->host);
 		$alerts[] = $row;
-//		echo "$row->triggerid";
 	}
+
+	/*
+	 * Carrega alertas nfdump
+	 */
+	
+	$sql = "SELECT description,priority,hostid FROM triggers WHERE value=1";
+	
+	$link = mysqli_connect($host, $username, $password, $dbNfdump);
+	$resultN= mysqli_query($link, $sql);
+  	while( $row = mysqli_fetch_object($resultN) )
+        {
+                $alerts[] = $row;
+        }	
+	
+	/*
+	 * Json encode
+	 */
+	
 	$json1 = '{"alerts":'.json_encode($alerts).'}';
-//	echo serialize($alerts[0]);
 	echo "$json1";
-	echo "\n\0";
-	$alertas = json_decode($json1);
 
-/*	foreach( $aler as $a )
-	{
-
-		echo "$a->priority";
-	}
-*/
 	mysqli_close($link);
 ?>
